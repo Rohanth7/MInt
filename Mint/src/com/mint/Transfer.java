bdc48bc7ce3b4501ae58cd9c608e7d674bb5ebb9
Rohanth <mrohanth@gmail.com>
2020-03-03 12:13:10 +0530

    Added fund transfer b/w both agent and customer

--- Mint/src/com/mint/Transfer.java
+++ Mint/src/com/mint/Transfer.java
@@ -46,12 +46,12 @@ public class Transfer extends HttpServlet
 			
 		
 			
-			String qry1="select COUNT(*) from customer Where Aadhar='"+aadhar+"' and Acc='"+acc+"' ";
+			String qry1="select Count(*) from (select * from customer where Aadhar='"+aadhar+"' and Acc='"+acc+"' union select * from agent where Aadhar='"+aadhar+"' and Acc='"+acc+"') as a;";
 			cred1=st1.executeQuery(qry1);
 			cred1.absolute(1);
 			
 
-			String qry2="select COUNT(*) from customer Where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"' ";
+			String qry2="select Count(*) from (select * from customer where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"' union select * from agent where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"') as a;";
 			cred2=st2.executeQuery(qry2);
 			cred2.absolute(1);
 
@@ -63,7 +63,7 @@ public class Transfer extends HttpServlet
 			
 			if(credcheck1.equals("1") && credcheck2.equals("1")) {
 				
-					String qry3="select * from customer Where Aadhar='"+aadhar+"' and Acc='"+acc+"' ";
+					String qry3="select * from customer Where Aadhar='"+aadhar+"' and Acc='"+acc+"' union select * from agent where  Aadhar='"+aadhar+"' and Acc='"+acc+"' ";
 					rs1=st3.executeQuery(qry3);
 					rs1.absolute(1);
 					balance1=Integer.parseInt(rs1.getString(3));
@@ -71,21 +71,29 @@ public class Transfer extends HttpServlet
 					if(balance1>=amount) {
 							balance1=balance1-amount;
 							String qry4="update customer set Amount='"+balance1+"' where Aadhar='"+aadhar+"' and Acc='"+acc+"'";
-							st3.executeUpdate(qry4);
-				
-				
-							String qry5="select * from customer Where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"'";
+							String qry44="update agent set Amount='"+balance1+"' where Aadhar='"+aadhar+"' and Acc='"+acc+"'";
+							
+							st3.addBatch(qry4);
+							st3.addBatch(qry44);
+							st3.executeBatch();
+							
+							
+							String qry5="select * from customer Where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"' union select * from agent where  Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"' ";
 							rs2=st4.executeQuery(qry5);
 							rs2.absolute(1);
-							
 							balance2=Integer.parseInt(rs2.getString(3));
+				
 							balance2=balance2+amount;
-							String qry6="update customer set Amount='"+balance2+"' Where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"'";
-							st4.executeUpdate(qry6);
+							String qry6="update customer set Amount='"+balance2+"' where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"'";
+							String qry66="update agent set Amount='"+balance2+"' where Aadhar='"+benefitaad+"' and Acc='"+benefitacc+"'";
+									
+							st4.addBatch(qry6);
+							st4.addBatch(qry66);
+							st4.executeBatch();
 				
 							out.print("<h3>Successfully Transferred");
 										}
-					else {out.print("<h3>Insuffiecient balance</h3>");}
+					else {out.print("<h3>Insufficient balance</h3>");}
 			
 						}
 			else {
